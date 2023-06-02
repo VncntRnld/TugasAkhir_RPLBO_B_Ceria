6dccdcce43a5fbe36ecf67f1a987cc9701ff3328
dhea <dhea.angelina@ti.ukdw.ac.id>
2023-06-02 14:45:55 +0700

    tgl saldo

--- src/main/java/com/mbanking/mbankingceria/infoSaldoController.java
+++ src/main/java/com/mbanking/mbankingceria/infoSaldoController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 import java.io.IOException;
 import java.net.URL;
@@ -20,13 +22,19 @@ public class infoSaldoController implements Initializable {
     private Button buttonBack;
     @FXML
     private Label labelSaldo;
+    @FXML
+    private Label labelTglSaldo;
 
     public void initialize(URL location, ResourceBundle resources) {
         // Jalan kalau ganti scene
         renderInfo();
     }
 
+
     private void renderInfo() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        labelTglSaldo.setText(dtf.format(now));
         labelSaldo.setText("Rp " + Data.akun.getSaldo());
     }
 
